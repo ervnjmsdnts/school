edc4a81e49f3c07ebe0b37eb5e71b5b709885ba9
ervnjmsdnts <earvinjamesdantes@gmail.com>
2024-04-28 20:05:28 +0800

    updated videos

--- src/lib/videos.ts
+++ src/lib/videos.ts
@@ -1,22 +1,22 @@
 export const videos = [
   {
     name: "PANGNGALAN",
-    url: "https://youtu.be/IeEhlEg6vw8?si=BNjRydajBMGSyT-c",
+    url: "https://www.youtube.com/embed/IeEhlEg6vw8?si=BQMslFAazIOVj1DB",
     topic: { subject: "filipino", quarter: "1st" },
   },
   {
     name: "MGA BAHAGI NG LIHAM",
-    url: "https://youtu.be/pZsPmkcE8Z8?si=L2T2R4vc_CGYKr20",
+    url: "https://www.youtube.com/embed/pZsPmkcE8Z8?si=v6jX737YFoHGuFs3",
     topic: { subject: "filipino", quarter: "2nd" },
   },
   {
     name: "PAGSASANAY BUMASA NG TATLONG PANTIG",
-    url: "https://youtu.be/pZsPmkcE8Z8?si=hJZu1gMZZdmNshsU",
+    url: "https://www.youtube.com/embed/pZsPmkcE8Z8?si=v6jX737YFoHGuFs3",
     topic: { subject: "filipino", quarter: "3rd" },
   },
   {
     name: "GAMIT NG PANGNGALAN",
-    url: "https://youtu.be/sURvNd8p9-s?si=1ODeTN3DQBguLGfb",
+    url: "https://www.youtube.com/embed/sURvNd8p9-s?si=LxBGCy66SvSxOrJs",
     topic: { subject: "filipino", quarter: "4th" },
   },
 
@@ -27,43 +27,43 @@ export const videos = [
   },
   {
     name: "NAMING SOUNDS",
-    url: "https://youtu.be/6h3V-Tm_tNs?si=NkO6Txd_CmNZS3FQ",
+    url: "https://www.youtube.com/embed/6h3V-Tm_tNs?si=LXo3ZM6Gx4PgGzFn",
     topic: { subject: "english", quarter: "2nd" },
   },
   {
     name: "NOUNS",
-    url: "https://youtu.be/qBVXUsEVCH8?si=r6lhytOm5p6bYypw",
+    url: "https://www.youtube.com/embed/6h3V-Tm_tNs?si=b851ZAeQqLeHOWPV",
     topic: { subject: "english", quarter: "3rd" },
   },
   {
     name: "DEMONSTRATIVE PRONOUNS",
-    url: "https://youtu.be/qBVXUsEVCH8?si=3Hhi3sjP5nBEsVDx",
+    url: "https://www.youtube.com/embed/qBVXUsEVCH8?si=JWnLg3k5fGVBUpK6",
     topic: { subject: "english", quarter: "4th" },
   },
 
   {
     name: "SKIP COUNTING BY 2,5 10",
-    url: "https://youtu.be/MEkx33ccSs4?si=zuR5tizi5U7HIDjO",
+    url: "https://www.youtube.com/embed/MEkx33ccSs4?si=Guq8rD1KcMQc4tzl",
     topic: { subject: "math", quarter: "1st" },
   },
   {
     name: "PHILIPPINE MONEY",
-    url: "https://youtu.be/myaQw2RIixo?si=DZTzC0hkwKyUDHeK",
+    url: "https://www.youtube.com/embed/myaQw2RIixo?si=NCi4vfXCoR8xWIjl",
     topic: { subject: "math", quarter: "1st" },
   },
   {
     name: "SUBTRACTION AND ADDITION",
-    url: "https://youtu.be/489srqkdgMQ?si=yM-Pxp7f6C_diwCa",
+    url: "https://www.youtube.com/embed/489srqkdgMQ?si=gkfxSEOD9P8YDdf9",
     topic: { subject: "math", quarter: "2nd" },
   },
   {
     name: "MULTIPLICATION",
-    url: "https://youtu.be/eW2dRLyoyds?si=PqrwKWQltrDjxhQs",
+    url: "https://www.youtube.com/embed/eW2dRLyoyds?si=q01Ez8EViD0uBD6u",
     topic: { subject: "math", quarter: "3rd" },
   },
   {
     name: "DIVISION",
-    url: "https://youtu.be/rGMecZ_aERo?si=s4wJqmirhDZEXbXd",
+    url: "https://www.youtube.com/embed/rGMecZ_aERo?si=R42p3RqqP49yUcak",
     topic: { subject: "math", quarter: "4th" },
   },
 ];
