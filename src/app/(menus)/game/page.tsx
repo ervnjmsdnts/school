6d5d07db7fc0795b02309a3921fb18b93bec113d
ervnjmsdnts <earvinjamesdantes@gmail.com>
2024-05-19 21:07:01 +0800

    game iframe

---
+++ src/app/(menus)/game/page.tsx
@@ -0,0 +1,12 @@
+export default function Games() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <iframe
+        src="https://www.turtlediary.com/game/what-are-nouns.html"
+        frameBorder="0"
+        allowFullScreen
+        className="h-full w-full"
+      />
+    </div>
+  );
+}
